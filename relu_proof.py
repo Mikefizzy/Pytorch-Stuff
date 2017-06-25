6f2a901e214577d86118aa8d26f4e4cedfbd44ca
Mikefizzy <mikefizzy@live.com>
2017-06-25 15:30:03 -0400

    added relu proof

---
+++ relu_proof.py
@@ -0,0 +1,17 @@
+#this script clearly shows the exponential growth in using relu
+import numpy as np
+import matplotlib.pyplot as plt
+layers = 4
+hidden_size  =100
+input_size = 2
+inputs = np.random.rand(1,input_size)
+maximums = []
+for i in range(layers):
+	w = np.random.rand(np.shape(inputs)[1], hidden_size)*2 -1 # weights are from a normal distribution [-1, 1]
+	l = np.matmul(inputs, w)
+	l = np.maximum(l, 0, l) #relu
+	l = l/np.max(l)
+	inputs = l;
+	maximums.append(np.max(l))
+plt.plot(maximums)
+plt.show()
